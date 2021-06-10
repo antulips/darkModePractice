0ace7ff3cb28f8708fe9f408b474cd0bc845244d
kushina <65832046+antulips@users.noreply.github.com>
2021-06-10 20:33:55 -0300

    [FIX] JS Optimization&Alternatives

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let bodyClass = document.querySelector('body')
 if (darkMode) {
 bodyClass.classList.toggle('dark');    
 }
+
+/* Para que se cambie constantemente.
+setInterval(() => {
+    body.classList.toggle('dark');
+}, 3000)*/
